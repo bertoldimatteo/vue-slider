c7d751e20da74b08d15e753df7bc3978f5b144cb
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-05-22 11:36:42 +0200

    tengo 1 thumb per il v-for e aggancio vue in html

--- js/main.js
+++ js/main.js
@@ -25,3 +25,9 @@ const images = [
         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     },
 ];
+
+// Partendo dal markup HTML allegato creare uno slider di immagini in VueJS.
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
